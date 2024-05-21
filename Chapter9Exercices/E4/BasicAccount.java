b0d14151d746ffa93df491cb717b1cb4c9c99d72
krishna3052002 <shukrishna23@gmail.com>
2024-05-21 20:21:42 +0600

    E4 Added

---
+++ Chapter9Exercices/E4/BasicAccount.java
@@ -0,0 +1,22 @@
+public class BasicAccount extends CheckingAccount {
+    public String name;
+    public int accountNumber;
+
+    public static void main(String[] args) {
+        BasicAccount account1=new BasicAccount();
+        account1.name="Krishna Saha";
+        account1.accountNumber=12345678;
+        account1.Deposit(10000);
+        System.out.println(account1.getCurrentBalance());
+        account1.getWithdraw(5000,"May",1);
+        System.out.println(account1.getCurrentBalance());
+        account1.Deposit(15000);
+
+        System.out.println(account1.getCurrentBalance());
+        account1.getWithdraw(5000,"May",2);
+        System.out.println(account1.getCurrentBalance());
+        account1.getWithdraw(5000,"June",1);
+        System.out.println(account1.getCurrentBalance());
+    }
+
+}
